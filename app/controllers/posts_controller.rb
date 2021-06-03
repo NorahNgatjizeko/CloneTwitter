9b91e779ec5e9fbc9fedb846615d9fb13981120b
norah ngatjizeko <tjeripo86@live.com>
2021-06-03 22:04:39 +0900

    new migration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,7 +64,7 @@ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Content.find(params[:id])
+      @post = Post.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_094017) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "post"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
